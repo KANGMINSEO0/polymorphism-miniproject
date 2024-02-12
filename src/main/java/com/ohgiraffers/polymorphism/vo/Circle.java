4b4f9a1e86b38ad0c4701b151a837af88e13e106
KANGMINSEO0 <juhee0629032@gmail.com>
2024-02-12 09:36:37 +0900

    commit

--- src/main/java/com/ohgiraffers/polymorphism/vo/Circle.java
+++ src/main/java/com/ohgiraffers/polymorphism/vo/Circle.java
@@ -1,4 +1,28 @@
 package com.ohgiraffers.polymorphism.vo;
 
-public class Circle {
+public class Circle extends Point{
+
+    private float radius;
+
+    public Circle() {}
+
+    public Circle(float x, float y, float radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
 }
--- src/main/java/com/ohgiraffers/polymorphism/vo/Line.java
+++ src/main/java/com/ohgiraffers/polymorphism/vo/Line.java
@@ -1,4 +1,28 @@
 package com.ohgiraffers.polymorphism.vo;
 
-public class Line {
+public class Line extends Point{
+
+    private float length;
+
+    public Line() {}
+
+    public Line(float x, float y, float length) {
+        super(x, y);
+        this.length = length;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "length=" + length +
+                '}';
+    }
 }
